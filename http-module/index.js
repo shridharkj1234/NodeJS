d6fc5a916239457dc1dd2888f1ebc46a75ef283a
Shridhar Kamble <GS-3168@GSLAB.COM>
2023-07-24 13:15:21 +0530

    http-module

--- http-module/index.js
+++ http-module/index.js
@@ -1,10 +1,19 @@
 const http = require("http");
-
-const server = http.createServer((req, res)=>{
-    res.writeHead(200,{"Content-type":"text/html"})
-    res.end("<h2> Hello From Node</h2> ")
+const fs = require("fs");
+const path = require("path");
+const data=fs.readFileSync(__dirname + "/data.json", "utf-8")
+const readHtml = fs.readFileSync(path.join(__dirname,'./public/index.html'),"utf-8")
+const server = http.createServer((req, res) => {
+  if (req.url === "/api") {
+      console.log(data);
+      res.writeHead(200, { "Content-type": "application/json" });
+      res.end(data);
+  } else if (req.url === "/") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(readHtml);
+  }
 });
 
-server.listen(8080,()=>{
-    console.log("Listening on Port 8080");
-})
+server.listen(8080, () => {
+  console.log("Listening on Port 8080");
+});
